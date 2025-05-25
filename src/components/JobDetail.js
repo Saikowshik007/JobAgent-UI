2e558f172bd2a57b2d456689058fca53f13648e8
Sai Kowshik Ananthula <ananthulasaikowshik@gmail.com>
2025-05-24 22:23:32 -0500

    added traefik support

--- src/components/JobDetail.js
+++ src/components/JobDetail.js
@@ -28,7 +28,7 @@ function JobDetail({ job, onStatusChange }) {
   useEffect(() => {
     setResumeError('');
     setResumeMessage('');
-    // Don't clear showStatusTracker here as it might interfere with resume generation
+    setShowStatusTracker(false);
     setUploadingToSimplify(false);
     setFetchingYamlForModal(false);
   }, [job.id]); // Clear when job ID changes
@@ -73,7 +73,7 @@ function JobDetail({ job, onStatusChange }) {
         !fetchingYaml.current) {
       fetchResumeYaml();
     }
-  }, [resumeId, resumeYaml, job.status, job.id]); // Added job.status and job.id to dependencies
+  }, [resumeId, resumeYaml]);
 
 const fetchResumeYaml = async () => {
   if (fetchingYaml.current || yamlFetched.current) {
